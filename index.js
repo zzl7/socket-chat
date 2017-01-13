31693385019cb31495f8ee862d2f6d95b97972b4
zzl7 <447999261@qq.com>
2017-01-13 18:10:34 +0800

    '首次提交代码'

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require("express")()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  /*res.send('<h1>Hello world</h1>');*/
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+	console.log("欢迎进入聊天室")
+	socket.on("chat message", function(msg){
+		console.log('message: ' + msg);
+	})
+	socket.on('disconnect', function(){
+	    console.log('欢迎下次再来哦！');
+	});
+});
